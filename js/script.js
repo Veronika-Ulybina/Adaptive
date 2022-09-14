48576029ba0cff90e77ecee53d1f5563fba10850
Veronika Ulybina <veronica.ulybina@yandex.ru>
2022-09-14 11:56:44 +0500

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+/*$(document).ready(function() {
+  $('.benefits__row').slick({
+    speed: 500,
+    infinite: false,
+    draggable: false,
+    waitForAnimate: false,
+  });
+});*/
+
+
+
+
+
+
+
