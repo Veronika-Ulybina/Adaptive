88b2e9580a08a8da7e79fb980ace908d45c470df
Veronika Ulybina <veronica.ulybina@yandex.ru>
2022-09-16 06:32:28 +0500

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 
-/*$(document).ready(function() {
-  $('.benefits__row').slick({
-    speed: 500,
-    infinite: false,
-    draggable: false,
-    waitForAnimate: false,
-  });
+/*let clientWidth = document.body.clientWidth;
+
+$('.benefits__row').slick({
+  speed: 500,
+  infinite: false,
+  draggable: false,
+  waitForAnimate: false,
+});
+
+function resizeTaleSlider(clientWidth) {
+  if (clientWidth < 480) {
+    $('.benefits__row').slick({
+      speed: 500,
+      infinite: false,
+      draggable: false,
+      waitForAnimate: false,
+    });
+  } else {
+    $('.benefits__row').slick("unslick");
+  }
+}
+
+resizeTaleSlider(clientWidth);
+
+$(window).resize(function() {
+  let clientWidth = document.body.clientWidth;
+  resizeTaleSlider(clientWidth);
 });*/
 
 
